2b9afb65b6983f91dc5942bf254e4339153bdd92
robguderian <rob.guderian@gmail.com>
2016-11-14 08:32:12 -0600

    Fixed errors. Still need to change NOTIFICATIONS to bot.memory

--- notifier.py
+++ notifier.py
@@ -12,14 +12,14 @@ def notify(channel, user, message):
         NOTIFICATIONS[channel][user] = []
     NOTIFICATIONS[channel][user].append(message)
 
-def checkNotifications(bot, user):
-    channel = bot.channel
-    if user in NOTIFICATIONS[channel]:
+def checkNotifications(bot, trigger, user):
+    channel = trigger.sender
+    if channel in NOTIFICATIONS and user in NOTIFICATIONS[channel]:
         for message in NOTIFICATIONS[channel][user]:
             prMessage = message
             NOTIFICATIONS[channel][user].remove(message)
-            bot.say("Notification for " + user + " : " + prMessage)
-        del NOTIFICATIONS[channel][user]        
+            bot.say("Notification for " + user + ": " + prMessage)
+        del NOTIFICATIONS[channel][user]
 
 def printHelp(bot):
     bot.say("Notifier:")
@@ -27,21 +27,22 @@ def printHelp(bot):
     bot.say("   .notify <nick> <message> - adds a message to give to user the next time they say something.")
 
 @sopel.module.commands('notify')
-def notify(bot, trigger):
-    channel = bot.channel
+def do_notify(bot, trigger):
+    channel = trigger.sender
     if channel in CHANNELS:
         words = trigger.group(2).split(' ')
-        if words[1] != trigger.nick:
-            notify(channel, words[1], trigger.group(2).replace(words[1], "", 1)
+        if words[0] != trigger.nick:
+            notify(channel, words[0],
+                    trigger.group(1).replace(words[0], "", 1))
+            bot.say('Added note for ' + words[0] + '.')
 
-@sopel.module.commands('.notifyhelp')
+@sopel.module.commands('notifyhelp')
 def notifyhelp(bot, trigger):
     printHelp(bot)
 
-@thread(False)
-@rule('(.*)')
-@priority('low')
+@sopel.module.thread(False)
+@sopel.module.rule('(.*)')
+@sopel.module.priority('low')
 def note(bot, trigger):
-    if bot.channel in CHANNELS:
-        checknotifications(bot, trigger.nick)
-        
+    if trigger.sender in CHANNELS:
+        checkNotifications(bot, trigger, trigger.nick)
